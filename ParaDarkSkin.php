77e9c78a14621520b60df5a7ffebe66c31d4841f
Sirryan2002 <80364400+Sirryan2002@users.noreply.github.com>
2021-05-31 21:04:06 -0400

    php scripts and shit

--- ParaDarkSkin.php
+++ ParaDarkSkin.php
@@ -9,7 +9,6 @@
  * also if I wanted to add references to JS scripts in this skin I could do that here
  */
 
-
  class SkinParaDark extends SkinTemplate { //We're extending the base MW skintemplate
     var $skinname = 'paradark', $stylename = 'ParaDark',
     $template = 'ParaDarkTemplate';
@@ -19,11 +18,29 @@
      * don't know what I'm doing to even a minimal extent
      */
 
-
     /**
+     * so this protected function SetupTemplates() took me a moment to understand (see:https://www.mediawiki.org/wiki/Manual:HTML_templates)
+     * what we're doing is using a constructor to build a template parser object, essentially what this does is
+     * allow us to get ready to read .mustache templates and implement them. a.k.a convert them into HTML for us
+     * this is the extent we work with this mustache templates on this php file though, the rest is handled in
+     * the template php
+     */
+ 
+    protected function SetupTemplates() {
+      $templateParser = new TemplateParser(  __DIR__ . 'includes/templates' ); //creating object and defining where mustache tempaltes are
+      echo $tp->processTemplate(
+        'MyWidget',
+        [
+            'username' => $user->getName(),
+            'message' => 'Hello!'
+        ]
+      );
+    }
+
+   /**
      * We're adding all of our CSS skins that we want applied via the resource loader
      * we don't need to specify all of them here b/c we've already done that in our skin.json file
-     */
+      */
     function setupSkinUserCss( OutputPage $out ) {
 		parent::setupSkinUserCss( $out );
 		$out->addModuleStyles( array(
--- ParaDarkTemplate.php
+++ ParaDarkTemplate.php
@@ -12,43 +12,377 @@
  * class needs to echo some html shit at some point
  */
 
-class FooBarTemplate extends BaseTemplate {
+class ParaDarkTemplate extends BaseTemplate {
 	/**
 	 * Outputs the entire contents of the page
      * also if you're reading this.... fuck you!
 	 */
+    protected function getTemplateParser() { //grab that fucking template parser
+		if ( $this->templateParser === null ) {
+			throw new \LogicException(
+				'TemplateParser has to be set first via setTemplateParser method'
+			);
+		}
+		return $this->templateParser;
+	}
+
+    /**
+     * essentially what we're doing here is calling almost every single functions our parent class feeds us
+     * it's combining all page data into one happy array that we'll use later.
+     */
+    private function getSkinData() : array {
+        $contentNavigation = $this->getSkin()->getMenuProps();
+		$skin = $this->getSkin();
+		$out = $skin->getOutput();
+		$title = $out->getTitle();
+        $mainPageHref = Skin::makeMainPageUrl();
+
+        $commonSkinData = $skin->getTemplateData() + [
+			'html-headelement' => $out->headElement( $skin ),
+			'page-isarticle' => (bool)$out->isArticle(),
+
+			// From OutputPage::getPageTitle, via ::setPageTitle().
+			'html-title' => $out->getPageTitle(),
+			'msg-tagline' => $skin->msg( 'tagline' )->text(),
+
+			'html-newtalk' => $newTalksHtml ? '<div class="usermessage">' . $newTalksHtml . '</div>' : '',
+
+			'msg-paradark-jumptonavigation' => $skin->msg( 'paradark-jumptonavigation' )->text(),
+			'msg-paradark-jumptosearch' => $skin->msg( 'paradark-jumptosearch' )->text(),
+
+			'html-printfooter' => $skin->printSource(),
+			'html-categories' => $skin->getCategories(),
+			'data-footer' => $this->getFooterData(),
+			'html-navigation-heading' => $skin->msg( 'navigation-heading' ),
+			'data-search-box' => $this->buildSearchProps(),
+
+			// Header
+			'data-logos' => ResourceLoaderSkinModule::getAvailableLogos( $this->getConfig() ),
+			'msg-sitetitle' => $skin->msg( 'sitetitle' )->text(),
+			'msg-sitesubtitle' => $skin->msg( 'sitesubtitle' )->text(),
+			'main-page-href' => $mainPageHref,
+
+			'data-sidebar' => $this->buildSidebar(),
+			'sidebar-visible' => $this->isSidebarVisible(),
+			'msg-paradark-action-toggle-sidebar' => $skin->msg( 'paradark-action-toggle-sidebar' )->text(),
+		] + $this->getMenuProps();
+
+        return $commonSkinData; //we gonna feed this shit to mustache soon enough
+    }
+
 	public function execute() {
 		$this->html( 'headelement' ); 
-        $this->text( 'sitename' );?>
+        $this->text( 'sitename' );
+        $tp = $this->getTemplateParser(); //grabbing our template parser
 
-        <a href="<?php // This outputs your wiki's main page URL to the browser.
-		    echo htmlspecialchars( $this->data['nav_urls']['mainpage']['href'] );
-            ?>"
-	        <?php echo Xml::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 'p-logo' ) ) 
-        ?>>
-            <img src="<?php 
-		 	    $this->text( 'logopath' ); 	
-		 	    // This outputs our stupid logo image
-		    ?>">
-        </a>
+        //8888888888888888888888888888888888888888
+        //8888888888888888888888888888888888888888
+        //8888888888888888888888888888888888888888
 
-        <?php
-        // 
-        if ( $this->data['title'] !== '' ) {
-	        // we're defining the attributes in our title first in this variables
-            // such as the ID/Class so we can refer to it in our CSS stylesheets later
-	        $titleAttribs = [ 'id' => 'firstHeading', 'class' => 'firstHeading' ];
-	        // Note that we don't use $this->html( 'title' ) here, because we don't want to echo the title at that point.
-	        echo Html::rawElement( 'h1', $titleAttribs, $this->data[ 'title' ] );
-            //methinks that echo is the syntax that throws html shitcode where it needs to go
-            //im going to bed now
+        private function getFooterData() : array {
+            $skin = $this->getSkin();
+            $footerRows = [];
+            foreach ( $this->getFooterLinks() as $category => $links ) {
+                $items = [];
+                $rowId = "footer-$category";
+    
+                foreach ( $links as $link ) {
+                    $items[] = [
+                        'id' => "$rowId-$link",
+                        'html' => $this->get( $link, '' ),
+                    ];
+                }
+    
+                $footerRows[] = [
+                    'id' => $rowId,
+                    'className' => null,
+                    'array-items' => $items
+                ];
+            }
+    
+            // If footer icons are enabled append to the end of the rows
+            $footerIcons = $this->getFooterIcons( 'icononly' );
+            if ( count( $footerIcons ) > 0 ) {
+                $items = [];
+                foreach ( $footerIcons as $blockName => $blockIcons ) {
+                    $html = '';
+                    foreach ( $blockIcons as $icon ) {
+                        $html .= $skin->makeFooterIcon( $icon );
+                    }
+                    $items[] = [
+                        'id' => 'footer-' . htmlspecialchars( $blockName ) . 'ico',
+                        'html' => $html,
+                    ];
+                }
+    
+                $footerRows[] = [
+                    'id' => 'footer-icons',
+                    'className' => 'noprint',
+                    'array-items' => $items,
+                ];
+            }
+    
+            $data = [
+                'array-footer-rows' => $footerRows,
+            ];
+    
+            return $data;
         }
-        $this->printTrail(); ?>
-</body>
-</html><?php 
-/** 
- * I think what this part does is close off the body and html tags of the page but dont
- * fucking quote me on that ok?
- */
+    }
+
+	private function isSidebarVisible() {
+		$skin = $this->getSkin();
+		if ( $skin->getUser()->isLoggedIn() ) {
+			return true //is user logged in? 
+		}
+		return false; //don't show sidebar when not logged in :)
+	}
+
+    private function buildSidebar() : array {
+		$skin = $this->getSkin();
+		$portals = $skin->buildSidebar();
+		$props = [];
+		$languages = null;
+
+		// Render portals
+		foreach ( $portals as $name => $content ) {
+			if ( $content === false ) {
+				continue;
+			}
+
+			// Numeric strings gets an integer when set as key, cast back - T73639
+			$name = (string)$name;
+
+			switch ( $name ) {
+				case 'SEARCH':
+					break;
+				case 'TOOLBOX':
+					$portal = $this->getMenuData(
+						'tb', $content, self::MENU_TYPE_PORTAL
+					);
+
+					$props[] = $portal
+					break;
+				case 'LANGUAGES':
+					$portal = $this->getMenuData(
+						'lang',
+						$content,
+						self::MENU_TYPE_PORTAL
+					);
+					// The language portal will be added provided either
+					// languages exist or there is a value in html-after-portal
+					// for example to show the add language wikidata link (T252800)
+					if ( count( $content ) || $portal['html-after-portal'] ) {
+						$languages = $portal;
+					}
+					break;
+				default:
+					// Historically some portals have been defined using HTML rather than arrays.
+					// Let's move away from that to a uniform definition.
+					if ( !is_array( $content ) ) {
+						$html = $content;
+						$content = [];
+						wfDeprecated(
+							"`content` field in portal $name must be array."
+								. "Previously it could be a string but this is no longer supported.",
+							'1.35.0'
+						);
+					} else {
+						$html = false;
+					}
+					$portal = $this->getMenuData(
+						$name, $content, self::MENU_TYPE_PORTAL
+					);
+					if ( $html ) {
+						$portal['html-items'] .= $html;
+					}
+					$props[] = $portal;
+					break;
+			}
+		}
+
+		$firstPortal = $props[0] ?? null;
+		if ( $firstPortal ) {
+			$firstPortal[ 'class' ] .= ' portal-first';
+		}
+
+		return [
+			'has-logo' => $this->isLegacy,
+			'html-logo-attributes' => Xml::expandAttributes(
+				Linker::tooltipAndAccesskeyAttribs( 'p-logo' ) + [
+					'class' => 'mw-wiki-logo',
+					'href' => Skin::makeMainPageUrl(),
+				]
+			),
+			'array-portals-rest' => array_slice( $props, 1 ),
+			'data-portals-first' => $firstPortal,
+			'data-portals-languages' => $languages,
+		];
+	}
+
+	/**
+	 * @param string $label to be used to derive the id and human readable label of the menu
+	 *  If the key has an entry in the constant MENU_LABEL_KEYS then that message will be used for the
+	 *  human readable text instead.
+	 * @param array $urls to convert to list items stored as string in html-items key
+	 * @param int $type of menu (optional) - a plain list (MENU_TYPE_DEFAULT),
+	 *   a tab (MENU_TYPE_TABS) or a dropdown (MENU_TYPE_DROPDOWN)
+	 * @param array $options (optional) to be passed to makeListItem
+	 * @param bool $setLabelToSelected (optional) the menu label will take the value of the
+	 *  selected item if found.
+	 * @return array
+	 */
+	private function getMenuData(
+		string $label,
+		array $urls = [],
+		int $type = self::MENU_TYPE_DEFAULT,
+		array $options = [],
+		bool $setLabelToSelected = false
+	) : array {
+		$skin = $this->getSkin();
+		$extraClasses = [
+			self::MENU_TYPE_DROPDOWN => 'vector-menu vector-menu-dropdown vectorMenu',
+			self::MENU_TYPE_TABS => 'vector-menu vector-menu-tabs vectorTabs',
+			self::MENU_TYPE_PORTAL => 'vector-menu vector-menu-portal portal',
+			self::MENU_TYPE_DEFAULT => 'vector-menu',
+		];
+		// A list of classes to apply the list element and override the default behavior.
+		$listClasses = [
+			// `.menu` is on the portal for historic reasons.
+			// It should not be applied elsewhere per T253329.
+			self::MENU_TYPE_DROPDOWN => 'menu vector-menu-content-list',
+		];
+		$isPortal = self::MENU_TYPE_PORTAL === $type;
+
+		// For some menu items, there is no language key corresponding with its menu key.
+		// These inconsitencies are captured in MENU_LABEL_KEYS
+		$msgObj = $skin->msg( self::MENU_LABEL_KEYS[ $label ] ?? $label );
+		$props = [
+			'id' => "p-$label",
+			'label-id' => "p-{$label}-label",
+			// If no message exists fallback to plain text (T252727)
+			'label' => $msgObj->exists() ? $msgObj->text() : $label,
+			'list-classes' => $listClasses[$type] ?? 'vector-menu-content-list',
+			'html-items' => '',
+			'is-dropdown' => self::MENU_TYPE_DROPDOWN === $type,
+			'html-tooltip' => Linker::tooltip( 'p-' . $label ),
+		];
+
+		foreach ( $urls as $key => $item ) {
+			// Add CSS class 'collapsible' to all links EXCEPT watchstar.
+			if (
+				$key !== 'watch' && $key !== 'unwatch' &&
+				isset( $options['vector-collapsible'] ) && $options['vector-collapsible'] ) {
+				if ( !isset( $item['class'] ) ) {
+					$item['class'] = '';
+				}
+				$item['class'] = rtrim( 'collapsible ' . $item['class'], ' ' );
+			}
+			$props['html-items'] .= $this->getSkin()->makeListItem( $key, $item, $options );
+
+			// Check the class of the item for a `selected` class and if so, propagate the items
+			// label to the main label.
+			if ( $setLabelToSelected ) {
+				if ( isset( $item['class'] ) && stripos( $item['class'], 'selected' ) !== false ) {
+					$props['label'] = $item['text'];
+				}
+			}
+		}
+
+		$props['html-after-portal'] = $isPortal ? $this->getAfterPortlet( $label ) : '';
+
+		// Mark the portal as empty if it has no content
+		$class = ( count( $urls ) == 0 && !$props['html-after-portal'] )
+			? 'vector-menu-empty emptyPortlet' : '';
+		$props['class'] = trim( "$class $extraClasses[$type]" );
+		return $props;
+	}
+
+	/**
+	 * @return array
+	 */
+	private function getMenuProps() : array {
+		// @phan-suppress-next-line PhanUndeclaredMethod
+		$contentNavigation = $this->getSkin()->getMenuProps();
+		$personalTools = $this->getPersonalTools();
+		$skin = $this->getSkin();
+
+		// For logged out users Vector shows a "Not logged in message"
+		// This should be upstreamed to core, with instructions for how to hide it for skins
+		// that do not want it.
+		// For now we create a dedicated list item to avoid having to sync the API internals
+		// of makeListItem.
+		if ( !$skin->getUser()->isLoggedIn() && User::groupHasPermission( '*', 'edit' ) ) {
+			$loggedIn =
+				Html::element( 'li',
+					[ 'id' => 'pt-anonuserpage' ],
+					$skin->msg( 'notloggedin' )->text()
+				);
+		} else {
+			$loggedIn = '';
+		}
+
+		// This code doesn't belong here, it belongs in the UniversalLanguageSelector
+		// It is here to workaround the fact that it wants to be the first item in the personal menus.
+		if ( array_key_exists( 'uls', $personalTools ) ) {
+			$uls = $skin->makeListItem( 'uls', $personalTools[ 'uls' ] );
+			unset( $personalTools[ 'uls' ] );
+		} else {
+			$uls = '';
+		}
+
+		$ptools = $this->getMenuData( 'personal', $personalTools );
+		// Append additional link items if present.
+		$ptools['html-items'] = $uls . $loggedIn . $ptools['html-items'];
+
+		return [
+			'data-personal-menu' => $ptools,
+			'data-namespace-tabs' => $this->getMenuData(
+				'namespaces',
+				$contentNavigation[ 'namespaces' ] ?? [],
+				self::MENU_TYPE_TABS
+			),
+			'data-variants' => $this->getMenuData(
+				'variants',
+				$contentNavigation[ 'variants' ] ?? [],
+				self::MENU_TYPE_DROPDOWN,
+				[], true
+			),
+			'data-page-actions' => $this->getMenuData(
+				'views',
+				$contentNavigation[ 'views' ] ?? [],
+				self::MENU_TYPE_TABS, [
+					'vector-collapsible' => true,
+				]
+			),
+			'data-page-actions-more' => $this->getMenuData(
+				'cactions',
+				$contentNavigation[ 'actions' ] ?? [],
+				self::MENU_TYPE_DROPDOWN
+			),
+		];
+	}
+
+	/**
+	 * @return array
+	 */
+	private function buildSearchProps() : array {
+		$config = $this->getConfig();
+		$skin = $this->getSkin();
+		$props = [
+			'form-action' => $config->get( 'Script' ),
+			'html-button-search-fallback' => $this->makeSearchButton(
+				'fulltext',
+				[ 'id' => 'mw-searchButton', 'class' => 'searchButton mw-fallbackSearchButton' ]
+			),
+			'html-button-search' => $this->makeSearchButton(
+				'go',
+				[ 'id' => 'searchButton', 'class' => 'searchButton' ]
+			),
+			'html-input' => $this->makeSearchInput( [ 'id' => 'searchInput' ] ),
+			'msg-search' => $skin->msg( 'search' ),
+			'page-title' => SpecialPage::getTitleFor( 'Search' )->getPrefixedDBkey(),
+		];
+		return $props;
 	}
 }
